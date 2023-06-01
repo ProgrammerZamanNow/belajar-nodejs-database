59f2d7fcd72041d6190d3913a5c5728a662a8279
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-06-01 22:27:42 +0700

    Model

--- database.sql
+++ database.sql
@@ -5,4 +5,18 @@ create table sample
     primary key (id)
 ) engine innodb;
 
-SELECT * FROM sample;
+SELECT *
+FROM sample;
+
+create table customers
+(
+    id    varchar(100) not null,
+    name  varchar(100) not null,
+    email varchar(100) not null,
+    phone varchar(100) not null,
+    primary key (id),
+    constraint customers_email_unique unique (email),
+    constraint customers_phone_unique unique (phone)
+) engine innodb;
+
+select * from customers;
